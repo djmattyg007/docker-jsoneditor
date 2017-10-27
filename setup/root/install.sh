bfa431cfdee07867403e2fe877a2d41cdbe7da54
Matthew Gamble <git@matthewgamble.net>
2017-10-27 19:31:25 +1100

    Avoid color and dynamic output from pacman

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -7,7 +7,7 @@ source /root/functions.sh
 
 # Install any packages
 echo "Installing nginx..."
-pacman -S --noconfirm nginx
+pacman -S --noconfirm --noprogressbar --color=never nginx
 mkdir -p /var/log/nginx
 chmod 775 /var/log/nginx && chown http:http /var/log/nginx
 ln -sf /dev/stdout /var/log/nginx/access.log
